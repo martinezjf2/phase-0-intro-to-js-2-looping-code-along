a221364bb0f0cd5a2d82a4ec559c84c561476f7f
Jeffrey Martinez <martinezjf2@gmail.com>
2021-12-15 16:09:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(array, feeling) {
+    let newArray = []
+    for (let i=0; i<array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${feeling} gift!`)
+    }
+    return newArray
+}
+
+
+function countDown(number) {
+    while (number > 0) { 
+        console.log(number)
+        number -= 1
+    }
+    console.log(number)
+}
